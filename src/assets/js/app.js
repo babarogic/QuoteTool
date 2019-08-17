07e85b3b720f87c7ec708458238eab6b9cf350e1
Goran Babarogic <gbabarogic@gmail.com>
2019-08-17 17:39:53 +0200

    Added new cards, footer, mobile navbar

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,11 +1,48 @@
-$("input[type='number']").inputSpinner();
-
-
-$(document).ready(function(){
-    $('.carousel').slick({
-    slidesToShow: 4,
-    dots:false,
+$(document).ready(function () {
+  $('.carousel').slick({
+    slidesToShow: 5,
+    dots: false,
     centerMode: false,
     arrows: true,
-    });
+    responsive: [{
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          arrows: false
+        }
+      }
+    ]
   });
+
+  $("input[type='number']").inputSpinner();
+
+  $('[data-cardSelect]').click(function () {
+    $(this).parent('[data-cardSelected]').toggleClass('selected');
+  });
+});
