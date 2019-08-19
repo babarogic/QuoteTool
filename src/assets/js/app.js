5061bd796d49289b7d61ac3bd7446d08587881c9
Goran Babarogic <gbabarogic@gmail.com>
2019-08-19 21:18:24 +0200

    product gallery added

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -40,22 +40,56 @@ $(document).ready(function () {
     ]
   });
 
+  $('.productSection').slick({
+    slidesToShow: 5,
+    dots: false,
+    centerMode: false,
+    arrows: true,
+    responsive: [{
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          arrows: false
+        }
+      }
+    ]
+  });
+
   $("input[type='number']").inputSpinner();
 
   $('[data-cardSelect]').click(function () {
     $(this).parent('[data-cardSelected]').toggleClass('selected');
   });
 
-  $('.trigger-button').click(function(e) {
+  $('.trigger-button').click(function (e) {
     e.stopPropagation();
     $('.dropdown-toggle').dropdown('toggle');
   });
 
-  // Show more
-
-  $(".showMore").click(function (e) {
-    e.preventDefault();
-    $(".open4").addClass("hide-team");
-    $(".team-open4").addClass("show-team");
-})
 });
